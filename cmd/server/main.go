87d199e869537c7e06c164462c60ea8dab7120da
gooneraki <hayiate4@hotmail.com>
2025-10-10 11:28:06 +0300

    Refactor RabbitMQ channel handling and JSON publishing in main.go and pubsub.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
@@ -22,20 +19,21 @@ func main() {
 	defer conn.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
 
-	newCha, err := conn.Channel()
+	publishCh, err := conn.Channel()
 	if err != nil {
-		log.Fatalf("could not create connection channel: %v", err)
+		log.Fatalf("could not create channel: %v", err)
 	}
 
-	koko, err := json.Marshal(routing.PlayingState{IsPaused: true})
+	err = pubsub.PublishJSON(
+		publishCh,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
 	if err != nil {
-		log.Fatalf("could not marshal playing state")
+		log.Printf("could not publish time: %v", err)
 	}
-	pubsub.PublishJSON(newCha, routing.ExchangePerilDirect, routing.PauseKey, koko)
-
-	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("RabbitMQ connection closed.")
+	fmt.Println("Pause message sent!")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -8,21 +8,12 @@ import (
 )
 
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
-
-	jsonVal, err := json.Marshal(val)
+	dat, err := json.Marshal(val)
 	if err != nil {
 		return err
 	}
-
-	if err := ch.PublishWithContext(
-		context.Background(),
-		exchange,
-		key,
-		false,
-		false,
-		amqp.Publishing{ContentType: "application/json", Body: jsonVal}); err != nil {
-		return err
-	}
-
-	return nil
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        dat,
+	})
 }
