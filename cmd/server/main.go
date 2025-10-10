b848ed298a67585cc95e1b22d247ceaa49543fe8
gooneraki <hayiate4@hotmail.com>
2025-10-10 11:26:26 +0300

    Implement JSON publishing to RabbitMQ in pubsub package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,6 +21,17 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	newCha, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not create connection channel: %v", err)
+	}
+
+	koko, err := json.Marshal(routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("could not marshal playing state")
+	}
+	pubsub.PublishJSON(newCha, routing.ExchangePerilDirect, routing.PauseKey, koko)
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
