45b52d48d9ac4045b4fb0c79b3c1ed446939204b
gooneraki <hayiate4@hotmail.com>
2025-10-14 10:12:30 +0300

    Add command handling for pause, resume, and exit in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -36,4 +37,53 @@ func main() {
 		log.Printf("could not publish time: %v", err)
 	}
 	fmt.Println("Pause message sent!")
+
+	gamelogic.PrintServerHelp()
+
+	for {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
+
+		switch input[0] {
+		case "pause":
+			fmt.Println("Sending pause message")
+			err = pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: true,
+				},
+			)
+			if err != nil {
+				log.Printf("could not publish time: %v", err)
+			}
+			fmt.Println("Pause message sent!")
+
+		case "resume":
+			fmt.Println("Sending resume message")
+			err = pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: false,
+				},
+			)
+			if err != nil {
+				log.Printf("could not publish time: %v", err)
+			}
+			fmt.Println("resume message sent!")
+		case "exit":
+			fmt.Println("Exiting")
+			return
+		default:
+			fmt.Printf("Don't understand command %s\n", input[0])
+
+		}
+
+	}
+
 }
