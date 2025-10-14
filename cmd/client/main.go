99ee43b6f7287b8ab2a334f1ea1fe6314fb0867e
gooneraki <hayiate4@hotmail.com>
2025-10-14 10:34:47 +0300

    Implement command handling for game actions in main.go

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -40,6 +40,43 @@ func main() {
 	}
 	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
 
+	newGame := gamelogic.NewGameState(username)
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+		switch words[0] {
+		case "spawn":
+			fmt.Println("Spawning ..")
+			err = newGame.CommandSpawn(words)
+
+			if err != nil {
+				log.Printf("could not spawn units: %v", err)
+			}
+		case "move":
+			fmt.Println("Moving ..")
+			_, err := newGame.CommandMove(words)
+			if err != nil {
+				log.Printf("could not publish time: %v", err)
+			}
+		case "status":
+			log.Println("Printing status ..")
+			newGame.CommandStatus()
+		case "help":
+			log.Println("Printing help ..")
+			gamelogic.PrintClientHelp()
+		case "spam":
+			log.Println("Spamming not allowed yet!")
+
+		case "quit":
+			gamelogic.PrintQuit()
+			return
+		default:
+			fmt.Println("unknown command")
+		}
+	}
+
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
